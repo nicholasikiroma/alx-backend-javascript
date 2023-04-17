2ef115765e5852df239b1e8b32089b3ef8d0cc41
Nicholas <nicholasikiroma@gmail.com>
2023-04-17 23:23:51 +0100

    task 1: block scope

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,10 @@
+export default function taskBlock(trueOrfalse) {
+    const task = false;
+    const task2 = true;
+
+    // eslint-disable-next-line no-empty
+    if (trueOrfalse) {
+    }
+
+    return [task, task2];
+}
