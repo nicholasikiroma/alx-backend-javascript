613cc8bfd72e90c028b12ae438b066fc41d0afbd
Nicholas <nicholasikiroma@gmail.com>
2023-04-17 23:49:46 +0100

    update package.json

--- 0x00-ES6_basic/.eslintrc.js
+++ 0x00-ES6_basic/.eslintrc.js
@@ -1,10 +1,14 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es2021": true
+        "es6": true
     },
     "extends": "eslint:recommended",
     "overrides": [
+        {
+            files: ['*.js'],
+            excludedFiles: 'babel.config.js'
+        }
     ],
     "parserOptions": {
         "ecmaVersion": "latest",
