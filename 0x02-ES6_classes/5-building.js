b4234f01edf19e8274e68a6a184fa32b1d41f8c4
Nicholas <nicholasikiroma@gmail.com>
2023-04-21 11:26:03 +0100

    fix(code  fail): implementation of abstract class

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,18 +1,19 @@
 export default class Building {
   constructor(sqft) {
-    if (
-      this.constructor !== Building
-      && typeof this.evacuationWarningMessage !== 'function'
-    ) {
-      throw new Error(
-        'Class extending Building must override evacuationWarningMessage'
-      );
-    }
-
     this._sqft = sqft;
+    this.evacuationWarningMessage();
   }
 
   get sqft() {
     return this._sqft;
+  }
+
+  set sqft(newSqft) {
+    this._sqft = newSqft
+  }
+
+  evacuationWarningMessage() {
+    if (this.constructor.name !== 'Building')
+    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
