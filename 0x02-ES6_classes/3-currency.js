1480eb8783aeaaed2ab02722cb78916bd4505a70
Nicholas <nicholasikiroma@gmail.com>
2023-04-21 09:44:35 +0100

    syntax fix

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -8,28 +8,28 @@ export default class Currency {
     const name = newName;
     if (typeof name === 'string') {
       this._name = name;
-    }
-    else {
-        throw new TypeError('Name must be a string');
+    } else {
+      throw new TypeError('Name must be a string');
     }
   }
+
   set code(newCode) {
     const code = newCode;
     if (typeof code === 'string') {
       this._code = code;
-    }
-    else {
-        throw new TypeError('Code must be a string')
+    } else {
+      throw new TypeError('Code must be a string');
     }
   }
 
   get name() {
     return this._name;
   }
+  
   get code() {
     return this._code;
   }
-  
+
   displayFullCurrency() {
     return `${this._name} (${this._code})`;
   }
